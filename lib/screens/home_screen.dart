5a867b04b1ec97bcb7b03f6fa47a01c0d7a987f6
Mohammed Abdul-Aziz <48477328+tajutechgh@users.noreply.github.com>
2025-10-08 17:31:13 +0200

    added the ui designs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,6 +13,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+         title: Text(
+           "Todo List",
+           style: TextStyle(
+             color: Colors.white,
+             fontSize: 24,
+             fontWeight: FontWeight.bold,
+           ),
+         ),
+        backgroundColor: Colors.purpleAccent,
+      ),
+      body: Column(
+        children: [
+          Text("Welcome To Todo List Home Page"),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          
+        },
+        backgroundColor: Colors.blueAccent,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
