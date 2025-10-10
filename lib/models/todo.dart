c53a5499af58c01cf3b6c059ab0d2ef7b8a6d847
Mohammed Abdul-Aziz <48477328+tajutechgh@users.noreply.github.com>
2025-10-10 19:24:30 +0200

    added todo model and services

---
+++ lib/models/todo.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+class Todo {
+
+  final String title;
+  final String description;
+  final bool completed;
+  final Int userId;
+
+  Todo({
+    required this.title,
+    required this.description,
+    required this.completed,
+    required this.userId
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      title: json['title'],
+      description: json['description'],
+      completed: json['completed'],
+      userId: json['userId'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'completed': completed,
+      'userId': userId,
+    };
+  }
+}
---
+++ lib/services/todo_service.dart
@@ -0,0 +1,182 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_app/models/todo.dart';
+
+class ApiService {
+
+  static const String baseUrl = "http://localhost:8080/api/v1/todos";
+
+  // GET all todos
+  static Future<List<Todo>> fetchAllTodos() async {
+
+    final response = await http.get(Uri.parse('$baseUrl/all'));
+
+    if (response.statusCode == 200) {
+
+      List body = jsonDecode(response.body);
+
+      return body.map((e) => Todo.fromJson(e)).toList();
+
+    } else {
+
+      throw Exception('Failed to load all todos');
+    }
+  }
+
+  // GET all completed todos
+  static Future<List<Todo>> fetchAllCompletedTodos() async {
+
+    final response = await http.get(Uri.parse('$baseUrl/completed'));
+
+    if (response.statusCode == 200) {
+
+      List body = jsonDecode(response.body);
+
+      return body.map((e) => Todo.fromJson(e)).toList();
+
+    } else {
+
+      throw Exception('Failed to load all completed todos');
+    }
+  }
+
+  // GET all pending todos
+  static Future<List<Todo>> fetchAllPendingTodos() async {
+
+    final response = await http.get(Uri.parse('$baseUrl/pending'));
+
+    if (response.statusCode == 200) {
+
+      List body = jsonDecode(response.body);
+
+      return body.map((e) => Todo.fromJson(e)).toList();
+
+    } else {
+
+      throw Exception('Failed to load all pending todos');
+    }
+  }
+
+  // POST create new todo
+  static Future<Todo> createTodo(Todo todo) async {
+
+    final response = await http.post(
+
+      Uri.parse('$baseUrl/create'),
+
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
+
+      body: jsonEncode(todo.toJson()),
+
+    );
+
+    if (response.statusCode == 201) {
+
+      return Todo.fromJson(jsonDecode(response.body));
+
+    } else {
+
+      throw Exception('Failed to create new todo');
+    }
+  }
+
+  // PUT update todo
+  static Future<Todo> updateTodo(int id, Todo todo) async {
+
+    final response = await http.put(
+
+      Uri.parse('$baseUrl/update/$id'),
+
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
+
+      body: jsonEncode(todo.toJson()),
+
+    );
+
+    if (response.statusCode == 200) {
+
+      return Todo.fromJson(jsonDecode(response.body));
+
+    } else {
+
+      throw Exception('Failed to update todo');
+    }
+  }
+
+  // DELETE todo
+  static Future<void> deleteTodo(int id) async {
+
+    final response = await http.delete(Uri.parse('$baseUrl/delete/$id'));
+
+    if (response.statusCode != 200) {
+
+      throw Exception('Failed to delete todo');
+    }
+  }
+
+  // PATCH check todo complete
+  Future<void> completeTodo(int id) async {
+
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token');
+
+    if (token == null) return;
+
+    final response = await http.patch(
+
+      Uri.parse('$baseUrl/complete/$id'),
+
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+
+      if (kDebugMode) {
+        print('Todo status updated successfully: ${response.body}');
+      }
+
+    } else {
+
+      if (kDebugMode) {
+        print('Failed to update todo status: ${response.statusCode} - ${response.body}');
+      }
+    }
+  }
+
+  // PATCH check todo pending
+  Future<void> pendingTodo(int id) async {
+
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token');
+
+    if (token == null) return;
+
+    final response = await http.patch(
+
+      Uri.parse('$baseUrl/incomplete/$id'),
+
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+
+      if (kDebugMode) {
+        print('Todo status updated successfully: ${response.body}');
+      }
+
+    } else {
+
+      if (kDebugMode) {
+        print('Failed to update todo status: ${response.statusCode} - ${response.body}');
+      }
+    }
+  }
+}
